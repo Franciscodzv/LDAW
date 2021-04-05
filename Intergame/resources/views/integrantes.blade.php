ba800d4e49658b3f2a62aab4065efa1ceb932a32
eduardo <A01704641@itesm.mx>
2021-04-05 16:27:43 -0500

    agregar vista integrantes completado

--- Intergame/resources/views/integrantes.blade.php
+++ Intergame/resources/views/integrantes.blade.php
@@ -35,7 +35,7 @@
       <img class="card-img-top" src="{{URL::asset('/image/cadena.jpeg')}}" alt="Eduardo Cadena" height="400px">
       <div class="card-body">
         <h5 class="card-title"> Eduardo Cadena Bernal A01704641</h5>
-        <p class="card-text">Tengo experiencia usando bootstrap, materialize,he desarrollado algunas landing page con PHP y Javascript, tengo conocimiento muy basico en angular</p>
+        <p class="card-text">Tengo experiencia usando bootstrap, materialize,he desarrollado algunas landing page con PHP y Javascript, tengo conocimiento muy basico en Angular</p>
         
       </div>
     </div>
@@ -44,7 +44,7 @@
       <img class="card-img-top" src="{{URL::asset('/image/paco.jpg')}}" alt="Francisco Diaz" height="400px">
       <div class="card-body">
         <h5 class="card-title">Francisco Agustin Diaz A01204695	</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <p class="card-text">Tengo experiencia en PHP, HTML, JavaScript y librerías de CSS como Bootstrap, Materialize, además de tener conocimiendo básico de Laravel.</p>
        
       </div>
     </div>
@@ -53,7 +53,7 @@
       <img class="card-img-top" src="{{URL::asset('/image/matute.jpg')}}" alt="Alberto Matute" height="400px">
       <div class="card-body">
         <h5 class="card-title">Alberto Matute Beltran A01704584</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <p class="card-text">Tengo experiencia con el uso de librerias de css como bootstrap y materialize, mi primera interaccion con los frameworks fue en el bloque 2 con laravel</p>
         
       </div>
     </div>
