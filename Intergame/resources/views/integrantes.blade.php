507f72b5deda8469e148ae0c98e25a9eb6743896
eduardo <A01704641@itesm.mx>
2021-04-28 14:56:38 -0500

    add header and footer

--- Intergame/resources/views/integrantes.blade.php
+++ Intergame/resources/views/integrantes.blade.php
@@ -1,32 +1,5 @@
-<!doctype html>
+@include('header')
 
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-
-  <title>Integrantes</title>
-  <meta name="description" content="The HTML5 Herald">
-  <meta name="author" content="SitePoint">
-
-  <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
-<link href="{{ asset('css/style.css') }}" rel="stylesheet">
-
-</head>
-
-<body>
-
-
-  <div class="container">
-      <nav class="navbar navbar-expand-lg">
-      <a class="navbar-brand" href="{{route('index') }}">InterGame</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-     
-    </nav>
   <h1 class="d-flex justify-content-center">Integrantes </h1> 
   <br>
   
@@ -60,8 +33,4 @@
    
     </div>
 
-  </div>
-  <script src="js/scripts.js"></script>
-
-</body>
-</html>
+@include('footer')
