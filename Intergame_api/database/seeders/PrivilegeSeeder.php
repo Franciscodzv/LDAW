f62a992c6e61f967685a301458244d3827413571
AlbertoMatuteB <a01704584@itesm.mx>
2021-05-25 13:01:12 -0500

    fix privilege 3

--- Intergame_api/database/seeders/PrivilegeSeeder.php
+++ Intergame_api/database/seeders/PrivilegeSeeder.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 
-class PermissionSeeder extends Seeder
+class PrivilegeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
